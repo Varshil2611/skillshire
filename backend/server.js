80752da7da62caa0ee20dbe156630f6c263c442d
Varshil Shah <varshilshah25@gmail.com>
2026-02-21 15:55:08 +0530

    Complete user authentication with JWT and bcrypt

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
 dotenv.config();
+connectDB();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth/",authRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Skillhire is running.");
 });
 const PORT = process.env.PORT || 3000;
-connectDB();
+
 app.listen(PORT, () => {
   console.log(`App Running on port ${PORT}`);
 });
